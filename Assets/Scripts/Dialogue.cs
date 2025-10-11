342ef7585d627b74f5066d3da271b5e9df3edff0
Jibril <jibrilchettaoui@gmail.com>
2025-10-11 14:10:42 +0200

    Première version de la fenetre de dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System;
+
+// https://www.youtube.com/watch?v=8oTYabhj248
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartLine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            } else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartLine()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = String.Empty;
+            StartCoroutine(TypeLine());
+        } else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
